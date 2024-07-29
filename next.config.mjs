2886ffa4068e2cc15111b89d0aaa5439f1102bb8
Marynryn <murmar95@gmail.com>
2024-07-30 00:51:28 +0300

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: "/carp-travel",
+  assetPrefix: "/carp-travel/",
+  trailingSlash: true,
+};
 
 export default nextConfig;
